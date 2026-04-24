b9a664c3b2785d64cc5d9d7a4b3bb678d72a755b
krishadoshi16 <202512112@dau.ac.in>
2026-04-24 21:56:10 +0530

    Seed database on deploy

--- EchoRoom_Postgres/backend/build.sh
+++ EchoRoom_Postgres/backend/build.sh
@@ -2,8 +2,12 @@
 # exit on error
 set -o errexit
 
-# No need to cd if rootDir is set in render.yaml
+# Navigate to backend
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Seed data if it's the first time
+# Using loaddata to restore your previous work
+python manage.py loaddata db_seed.json || echo "Data already seeded or error"
